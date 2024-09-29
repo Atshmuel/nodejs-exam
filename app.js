ed14dd670836fc7c74b7a17b275aae1f54696262
Shmuel atar <shmuelatar@gmail.com>
2024-09-29 23:41:23 +0300

    Added home page to allow ease navigation on landing to choose if user is manager or guard

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-// בחברת שמירה שעושה סיורים רוצים לתעד את ביקורי השומרים בנקודות השונות, ולשם כך מבקשים ממך לכתוב תוכנה שעונה לאפיונים הבאים:
 
 // הנקודות ניתנות לניהול, כלומר התוכנה מאפשרת להוסיף, לערוך למחוק ולראות את רשימת כל הנקודות הקיימות
-
-// לשם כך נדרש ליצור גם עמודי צד קדמי וכן תמיכת צד שרת
-
 // בנוסף, יש ליצור עמוד שמאפשר הזנת ביקור בנקודה, שבו בוחרים את הנקודה שביקרנו בה. זמן הביקור נשמר אוטומטית
 
 const bodyParser = require('body-parser')
@@ -20,10 +16,25 @@ app.listen(port, () => {
     console.log(`listen on url: http://localhost:${port}/`);
 })
 
+const gourds = [];
+const points = []
+const visits = []
+// {gruadId:0,pointId:0,visitTime:0}
+
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "main.html"))
+
+})
+app.get('/manager', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "manager.html"))
+})
+app.get('/guard', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "guard.html"))
 })
-// app.get('/', (req, res) => { })
-// app.get('/', (req, res) => { })
-// app.get('/', (req, res) => { })
-// app.get('/', (req, res) => { })
+// app.get('/manager/points', (req, res) => { })
+// app.post('/manager/point', (req, res) => { })
+// app.patch('/manager/point', (req, res) => { })
+// app.delete('/manager/point', (req, res) => { })
+
+
+// app.post('guard/visit/:pointId', (req, res) => { })
